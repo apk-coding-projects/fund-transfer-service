ce726e77dceb7603a09c15cf4de518c5e61d97ff
Arturs Kosparenko <akosparenko@gmail.com>
2023-12-11 20:39:06 +0200

    hotfixes

--- src/Transactions/Repositories/TransactionRepository.php
+++ src/Transactions/Repositories/TransactionRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace src\Transactions\Repositories;
 
+use src\Accounts\Models\Account;
 use src\Transactions\Models\Transaction;
 
 class TransactionRepository
@@ -11,5 +12,24 @@ class TransactionRepository
     public function save(Transaction $transaction): void
     {
         $transaction->save();
+    }
+
+    /** @return Account[] */
+    public function getPaginatedByAccountId(int $accountId, int $limit, int $offset): array
+    {
+        $query = Transaction::
+        where('receiver_account_id', $accountId)
+            ->orWhere('sender_account_id', $accountId)
+            ->orderByDesc('id');
+
+        if ($limit) {
+            $query->limit($limit);
+        }
+
+        if ($offset) {
+            $query->offset($offset);
+        }
+
+        return $query->get()->all();
     }
 }
